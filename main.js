7317403353e875547dfdc0b4058d0d94015e2133
A-childs-encyclopedia <84061672+A-childs-encyclopedia@users.noreply.github.com>
2025-01-13 12:20:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ blog({
     { title: "Email", url: "mailto:abdhllah005@gmail.com" },
     { title: "GitHub", url: "https://github.com/A-childs-encyclopedia" },
   ],
-  background: "#ffde59",
+  background: "#650ab0",
   middlewares: [
     ga("UA-91675022-1"),
     redirects({
